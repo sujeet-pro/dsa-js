1de7b9b982ff1d1cfc68022db3b41cb38d6bb749
Sujeet <contact@sujeet.pro>
2023-11-06 14:57:43 +0530

    Problem(New): Sort Colors

---
+++ 01-two-pointers/sort-colors/sort-colors.solution.ts
@@ -0,0 +1,39 @@
+
+// type Tcolor = number | 0 (red)| 1 (white)| 2(blue)
+
+const RED = 0
+const WHITE = 1
+const BLUE = 2
+
+function swap(arr: number[], i: number, j: number) {
+  if (i >= 0 && j >= 0 && i < arr.length && j < arr.length) {
+    const temp = arr[i] as number
+    arr[i] = arr[j] as number
+    arr[j] = temp
+  }
+}
+
+export function sortColors(colors: number[]): number[] {
+  let red = 0
+  let white = 0
+  let blue = colors.length - 1
+
+  while (blue >= white) {
+    switch (colors[white]) {
+      case WHITE:
+        white += 1
+        break;
+      case RED:
+        swap(colors, white, red)
+        red += 1
+        white += 1
+        break;
+      case BLUE:
+        swap(colors, white, blue)
+        blue -= 1
+        break
+    }
+  }
+
+  return colors
+}
---
+++ 01-two-pointers/sort-colors/sort-colors.test.ts
@@ -0,0 +1,40 @@
+import { sortColors } from "./sort-colors.solution"
+
+function matchColors(actual: number[], expected: number[]) {
+  expect(actual).toHaveLength(expected.length) // Checks length
+  expect(actual).toEqual(expect.arrayContaining(expected)) // Check for all the elements 
+  expect(actual.toString()).toBe(expected.toString()) // Ensures order
+}
+
+describe('Two Pointer: Sort Color', () => {
+
+  test('Empty', () => {
+    matchColors(sortColors([]), [])
+  })
+  test('Single Color', () => {
+    const testCases = [
+      [0, 0, 0],
+      [1, 1, 1],
+      [2, 2, 2],
+    ]
+    testCases.forEach(testCase => {
+      matchColors(sortColors(testCase), testCase)
+    })
+  })
+
+  test('Multi Color', () => {
+    const testCases = [
+      [0, 1, 0, 1],
+      [0, 1, 2, 0, 1],
+      [0, 1, 0],
+      [1, 1, 0, 2],
+      [2, 1, 1, 0, 0],
+      [2, 2, 2, 0, 1, 0],
+      [2, 1, 1, 0, 1, 0, 2]
+    ] as const
+
+    testCases.forEach(testCase => {
+      matchColors(sortColors([...testCase]), [...testCase].sort())
+    })
+  })
+})
